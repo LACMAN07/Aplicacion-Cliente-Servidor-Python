546ac1974232c373590a2cd7c78cb4d6fc0870b3
LACMAN07 <32080122+LACMAN07@users.noreply.github.com>
2018-11-01 11:14:59 -0600

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -10,17 +10,14 @@ def main():
         
     # Escuchar peticiones en el puerto 6030.
     s.bind(("localhost", 6000))
-    s.listen(0)
-        
+    s.listen(0)  
     conn, addr = s.accept()
     nombre = str(raw_input("nombre de archivo a guardar: "))
     archivoEnviar = open(nombre, "wb")
-    contador = 1    
-    while archivoEnviar == archivoEnviar:
+    while True:
         try:
             # Recibir datos del cliente.
             input_data = conn.recv(1024)
-            
         except error:
             print("Error de lectura.")
             break
@@ -28,18 +25,15 @@ def main():
             if input_data:
                 # Compatibilidad con Python 3.
                 if isinstance(input_data, bytes):
-                    end = input_data[0] == 1
-                    
+                    end = input_data[0] == 1   
                 else:
                     end = input_data == chr(1)
-                
                 if not end:
                     # Almacenar datos.
                     archivoEnviar.write(input_data)
-               
                 else:
                     break
-        contador = contador+1
+        
         
     print("El archivo se ha recibido correctamente.")
     archivoEnviar.close()
